435bbb28a432f3dbb3724331451e37068e5b8e57
FangliZhan <31291225+FangliZhan@users.noreply.github.com>
2021-07-26 15:27:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"    
+    organization = "dcatcher-learning"    
     workspaces {
       name = "learn-terraform-pipelines-k8s"
     }
